838b442d4411a04acc8ccdf67a3ea99b0b70baf2
Girts Silis <girts.silis@gmail.com>
2026-02-20 11:19:10 -0600

    + export values and suits

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,28 @@
-import { Card } from "./cards/card";
+import { ACE, Card, CLUB, DIAMOND, EIGHT, FIVE, FOUR, HEART, JACK, KING, NINE, QUEEN, SEVEN, SIX, SPADE, TEN, THREE, TWO } from "./cards/card";
 import { Deck } from "./cards/deck";
 
 export default {
   Deck,
   Card,
+  Values: {
+    TWO,
+    THREE,
+    FOUR,
+    FIVE,
+    SIX,
+    SEVEN,
+    EIGHT,
+    NINE,
+    TEN,
+    JACK,
+    QUEEN,
+    KING,
+    ACE,
+  },
+  Suits: {
+    CLUB,
+    DIAMOND,
+    HEART,
+    SPADE,
+  }
 }
